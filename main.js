e5c9a7f98eab1eaff190dc8736e9d6fb9bbf2dca
Ha-g <you@example.com>
2020-03-20 21:34:04 -0700

    initial typescript

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Welcome back';
+console.log(message);
+var x = 10;
+var y = 20;
+var isBeginner = true;
+var total = 27;
+var name = 'Abdi';
+var sentence = "My name is " + name + "\nI am " + total + " years old";
+console.log(sentence);
+var n = null;
+var u = undefined;
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['Chris', 22, 6];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Blue;
+console.log(c);
---
+++ main.ts
@@ -0,0 +1,30 @@
+export {}
+let message = 'Welcome back';
+console.log(message);
+
+let x = 10;
+
+const y = 20;
+
+
+let isBeginner: boolean = true;
+let total: number = 27;
+let name: string = 'Abdi';
+
+let sentence: string = `My name is ${name}
+I am ${total} years old`
+
+console.log(sentence)
+
+let n: null = null;
+let u: undefined = undefined;
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+let person1: [string, number, number] = ['Chris', 22, 6];
+
+enum Color {Red, Green, Blue}
+
+let c: Color = Color.Blue
+console.log(c)
